384dba0f4aa889b9f1e4f0f7a7a2e42775141fb2
Code3rAvailable <113391386+Cod3rAvailable@users.noreply.github.com>
2022-10-10 18:49:15 +0300

    Add files via upload

---
+++ StickManWoman/StickWomanProject.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t _|_ \t \t\n";  
+cout << " \t \t  | \t \t \n";
+cout << " \t \t / L \t \t \n \n";
+
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t _|_ \t \t\n";  
+cout << " \t \t  V \t \t \n";
+cout << " \t \t / L \t \t \n \n";
+
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t<_|_> \t \t\n";  
+cout << " \t \t  V \t \t \n";
+cout << " \t \t / L \t \t";
+
+cout << "\t \t  \t\t \n \n"; 
+
+cout << " \t \t  O \t \t \n";
+cout << " \t \t _|_ \t \n";
+cout << " \t \t <_> \t \n";
+cout << " \t \t  V \t \n";
+cout << " \t \t / L\t \n";
+
+cout << "\t \t  \t\t \n \n"; 
+
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t _|_ \t \t\n";  
+cout << " \t \t  | \t \t \n";
+cout << " \t \t  V \t \t \n";
+cout << " \t \t / L \t \t\n\n" ;
+
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t _|_ \t \t\n";  
+cout << " \t \t  | \t \t \n";
+cout << " \t \t / L \t \t\n\n";
+
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t _|_ \t \t\n";  
+cout << " \t \t  8 \t \t \n";
+cout << " \t \t / L \t \t \n\n";
+
+cout << " \t \t  O \t \t \n"; 
+cout << " \t \t _|_ \t \t\n";  
+cout << " \t \t  & \t \t \n";
+cout << " \t \t / L \t \t";
+
+}
